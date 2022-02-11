1142c98901e55263c17736cd4d16cd7238b3dccd
QurbanovTM <Tarlan.Qurbanov@kapitalbank.az>
2022-02-11 17:09:39 +0400

    Change logic of api reverse and api purchase

--- src/main/java/az/kapitalbank/marketplace/constants/OrderStatus.java
+++ src/main/java/az/kapitalbank/marketplace/constants/OrderStatus.java
@@ -7,13 +7,6 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public enum OrderStatus {
-    SUCCESS(1),
-    FAIL(2),
-    ERROR(0);
-
-    int status;
-
-    public int getStatus() {
-        return status;
-    }
+    SUCCESS,
+    FAIL;
 }
