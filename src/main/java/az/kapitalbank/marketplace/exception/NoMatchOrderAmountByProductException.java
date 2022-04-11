6be1cd443ecca2e25b215115b9bb2166f4ac547f
MikayilovOE <orxan.mikayilov@kapitalbank.az>
2022-04-11 14:28:42 +0400

    bugfix

--- src/main/java/az/kapitalbank/marketplace/exception/NoMatchOrderAmountByProductException.java
+++ src/main/java/az/kapitalbank/marketplace/exception/NoMatchOrderAmountByProductException.java
@@ -5,7 +5,8 @@ import java.math.BigDecimal;
 public class NoMatchOrderAmountByProductException extends RuntimeException {
 
     private static final String MESSAGE =
-            "Order amount is not equal total product amount. orderAmount=%s , productsTotalAmount=%s";
+            "Order amount is not equal total product amount."
+                    + " orderAmount=%s , productsTotalAmount=%s";
 
     public NoMatchOrderAmountByProductException(BigDecimal orderAmount,
                                                 BigDecimal productsTotalAmount) {
