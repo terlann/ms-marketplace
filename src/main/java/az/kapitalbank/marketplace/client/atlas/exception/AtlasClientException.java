7ce88b6c28aac601c364c503c043b81cac852020
MikayilovOE <orxan.mikayilov@kapitalbank.az>
2022-04-21 12:56:55 +0400

    bugfix for prepurchase consumer

--- src/main/java/az/kapitalbank/marketplace/client/atlas/exception/AtlasClientException.java
+++ src/main/java/az/kapitalbank/marketplace/client/atlas/exception/AtlasClientException.java
@@ -1,7 +1,9 @@
 package az.kapitalbank.marketplace.client.atlas.exception;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
 
+@Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class AtlasClientException extends RuntimeException {
     private final String uuid;
