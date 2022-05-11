a19513a276f452adde8e3391f28f610f24a1e3fd
MikayilovOE <orxan.mikayilov@kapitalbank.az>
2022-05-11 14:51:32 +0400

    NPO-492 bugfix for existsByCustomerIdAndUmicoDecisionStatuses

--- src/main/java/az/kapitalbank/marketplace/service/OrderService.java
+++ src/main/java/az/kapitalbank/marketplace/service/OrderService.java
@@ -7,9 +7,6 @@ import static az.kapitalbank.marketplace.constant.CommonConstant.CUSTOMER_ID_LOG
 import static az.kapitalbank.marketplace.constant.CommonConstant.ORDER_NO_LOG;
 import static az.kapitalbank.marketplace.constant.CommonConstant.TELESALES_ORDER_ID_LOG;
 import static az.kapitalbank.marketplace.constant.UmicoDecisionStatus.APPROVED;
-import static az.kapitalbank.marketplace.constant.UmicoDecisionStatus.FAIL_IN_PENDING;
-import static az.kapitalbank.marketplace.constant.UmicoDecisionStatus.FAIL_IN_PREAPPROVED;
-import static az.kapitalbank.marketplace.constant.UmicoDecisionStatus.PENDING;
 import static az.kapitalbank.marketplace.constant.UmicoDecisionStatus.PREAPPROVED;
 
 import az.kapitalbank.marketplace.client.atlas.AtlasClient;
@@ -205,8 +202,7 @@ public class OrderService {
 
     private void checkCustomerIncompleteProcess(CustomerEntity customerEntity) {
         var isExistsCustomerByDecisionStatus = operationRepository
-                .existsByCustomerIdAndUmicoDecisionStatuses(customerEntity.getId().toString(),
-                        List.of(PENDING, FAIL_IN_PENDING, PREAPPROVED, FAIL_IN_PREAPPROVED));
+                .existsByCustomerIdAndUmicoDecisionStatuses(customerEntity.getId().toString());
         if (isExistsCustomerByDecisionStatus) {
             throw new CustomerNotCompletedProcessException(
                     "customerId - " + customerEntity.getId());
