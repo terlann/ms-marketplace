99439bc4f0384c457ebdbaf37b39b5b599cab51d
QurbanovTM <Tarlan.Qurbanov@kapitalbank.az>
2022-05-12 12:56:53 +0400

    NPO-477 send info sms after send web url to umico

--- src/main/java/az/kapitalbank/marketplace/config/SmsProperties.java
+++ src/main/java/az/kapitalbank/marketplace/config/SmsProperties.java
@@ -3,24 +3,20 @@ package az.kapitalbank.marketplace.config;
 import java.util.HashMap;
 import java.util.Map;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.context.scope.refresh.RefreshScopeRefreshedEvent;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Getter
-@Builder
 @RefreshScope
 @Component
 @ConfigurationProperties("sms")
-@PropertySource(value = "classpath:/config/sms.yml")
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class SmsProperties {
     Map<String, String> values = new HashMap<>();
