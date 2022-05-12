adde9e7e2ab7a7db044264d98f779795c1f6e50a
MikayilovOE <orxan.mikayilov@kapitalbank.az>
2022-05-12 12:28:04 +0400

    NPO-473 some changes

--- src/main/java/az/kapitalbank/marketplace/controller/SchedulerController.java
+++ src/main/java/az/kapitalbank/marketplace/controller/SchedulerController.java
@@ -1,16 +1,19 @@
 package az.kapitalbank.marketplace.controller;
 
 import az.kapitalbank.marketplace.scheduler.RefundSchedule;
+import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/schedule")
+@RequestMapping("/api/v1/scheduler")
 @RequiredArgsConstructor
-public class ScheduleController {
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class SchedulerController {
 
     private final RefundSchedule refundSchedule;
 
