5e00874217044844f121052915e97e53c8215f0a
fuad <fuad.ahmadov@kapitalbank.az>
2022-05-23 10:23:42 +0400

    NPO-524 refactored sms request and response

--- src/main/java/az/kapitalbank/marketplace/client/common/model/request/SendSmsRequest.java
+++ src/main/java/az/kapitalbank/marketplace/client/common/model/request/SendSmsRequest.java
@@ -1,15 +1,18 @@
 package az.kapitalbank.marketplace.client.common.model.request;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class SendSmsRequest {
-    private String body;
-    private String phoneNumber;
+    String body;
+    String phoneNumber;
 }
