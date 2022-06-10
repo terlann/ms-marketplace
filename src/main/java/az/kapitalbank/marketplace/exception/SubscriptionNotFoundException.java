843a7b03923eb703322a6d4e8573fb7120c59da1
QurbanovTM <Tarlan.Qurbanov@kapitalbank.az>
2022-06-10 09:13:15 +0400

    NPO-562 refactored exception in marketplace

--- src/main/java/az/kapitalbank/marketplace/exception/SubscriptionNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package az.kapitalbank.marketplace.exception;
-
-public class SubscriptionNotFoundException extends RuntimeException {
-    static final String SUBSCRIPTION_NOT_FOUND = "Card UID related mobile number not found. %s";
-
-    public SubscriptionNotFoundException(String message) {
-        super(String.format(SUBSCRIPTION_NOT_FOUND, message));
-    }
-}
