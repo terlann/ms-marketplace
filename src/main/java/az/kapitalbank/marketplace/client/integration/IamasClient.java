106c8e9c2e52eefc0759349db1a1dbc3b2c97cc6
QurbanovTM <Tarlan.Qurbanov@kapitalbank.az>
2022-06-13 12:42:55 +0400

    NPO-605 added security

--- src/main/java/az/kapitalbank/marketplace/client/integration/IamasClient.java
+++ src/main/java/az/kapitalbank/marketplace/client/integration/IamasClient.java
@@ -8,13 +8,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "iamas",
-        url = "${client.iamas.url}",
+@FeignClient(name = "integration",
+        url = "${client.integration.url}",
         primary = false,
         configuration = IamasClient.FeignConfiguration.class)
 public interface IamasClient {
 
-    @GetMapping("/id-card/by/pin/{pin}")
+    @GetMapping("/iamas/id-card/by/pin/{pin}")
     List<IamasResponse> findPersonByPin(@PathVariable("pin") String pin);
 
     class FeignConfiguration {
