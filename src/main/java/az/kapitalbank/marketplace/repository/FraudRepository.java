462519e59ae414bd3673661d6b1560c14996c166
MikayilovOE <orxan.mikayilov@kapitalbank.az>
2022-08-17 15:00:35 +0400

    NPO-797

--- src/main/java/az/kapitalbank/marketplace/repository/FraudRepository.java
+++ src/main/java/az/kapitalbank/marketplace/repository/FraudRepository.java
@@ -4,12 +4,14 @@ import az.kapitalbank.marketplace.entity.FraudEntity;
 import java.util.Set;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FraudRepository extends JpaRepository<FraudEntity, UUID> {
 
+    @Modifying
     @Query(nativeQuery = true,
             value = "UPDATE KB_MARKETPLACE_FRAUD "
                     + "SET is_deleted = 1 "
